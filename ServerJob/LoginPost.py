1343fb1b83d357fd63299a9b3038c987cca7eaf8
lyMeiSEU <213173625@seu.edu.cn>
2019-09-10 17:12:32 +0800

    10/09/2019 server
    server and webcontroler

--- ServerJob/LoginPost.py
+++ ServerJob/LoginPost.py
@@ -14,21 +14,22 @@ data = {
     "userName": 213173625
 }
 
-sess = requests.session()
 checkUrl = 'http://xk.urp.seu.edu.cn/studentService/system/login.action'
 codeurl = 'http://xk.urp.seu.edu.cn/studentService/getCheckCode'
 
 
 class checkSchoolServer(object):
+   
     def __init__(self):
         try:
-            sess.get(checkUrl)
-            self.valcode = sess.get(codeurl)
-            f = open('vercode.jpg', 'wb')
+            self.sess = requests.session()
+            self.sess.get(checkUrl)
+            self.valcode = self.sess.get(codeurl) # 获取valcode并维护session
+            #f = open('vercode.jpg', 'wb')
             # 将response的二进制内容写入到文件中
-            f.write(self.valcode.content)
+            #f.write(self.valcode.content)
             # 关闭文件流对象
-            f.close()
+            #f.close()
         except Exception as e:
             print(e)
 
@@ -38,25 +39,24 @@ class checkSchoolServer(object):
             data["password"] = str(password)
             data["userName"] = str(userName)
             # 传输用户名，密码，验证码并得到学校返回
-            r = sess.post(checkUrl, data=data)
+            r = self.sess.post(checkUrl, data=data)
             # 从返回中找到alertError错误字段，识别错误类型
             where = r.text.find('<body onload="alertError')
             if where == -1:
                 return True
             tex = r.text[where:where+50]
             print(tex)
+            # 判断错误情况
             if tex.find("验证码") != -1:
-                print("vercodeError")
                 return "vercodeError"
             elif tex.find("用户名") != -1:
-                print("passwordError")
                 return "passwordError"
             return False
         except Exception as e:
-            print(e)
+            print("Login Post error"+e)
             return False
 
 
-if __name__ == "__main__":
-    school = checkSchoolServer()
-    school.checkLogin()
+# if __name__ == "__main__":
+#    school = checkSchoolServer()
+#    school.checkLogin()
--- ServerJob/server.py
+++ ServerJob/server.py
@@ -2,11 +2,15 @@
 from globalManager import gol
 from LoginPost import checkSchoolServer
 from sqlUtil import MysqlUtil
+from multiprocessing import Process, Pool
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from collections import defaultdict
+from threading import Thread
+from collections import defaultdict
+import multiprocessing
+import os
 import json
 import string
-import socketserver
 import pymysql
 import threading
 import sys
@@ -15,67 +19,78 @@ sys.path.append('./*')
 
 # 服务端
 
+mysqlutil = MysqlUtil()
+dic_cookieHandler = defaultdict(list)
+
 
 class MyServer(BaseHTTPRequestHandler):
     # 从a-zA-Z0-9生成指定数量的随机字符做为cookie
-    ran_str_cookie = ''.join(random.sample(
-        string.ascii_letters + string.digits, 24))
     # 处理get请求
 
     def do_GET(self):
-        print("收到来自:"+self.client_address[0]+"的连接")
-        schookCheck = checkSchoolServer()
-        gol.set_schoolCheck(schookCheck)
+        global mysqlutil
         self.send_response(200)
-        self.ran_str_cookie = ''.join(random.sample(
+        ran_str_cookie = ''.join(random.sample(
             string.ascii_letters + string.digits, 24))
+        dic_cookieHandler[self.client_address[0]].append(ran_str_cookie)
         # 发送响应头部并附带cookie
-        self.send_header('cookie: '+self.ran_str_cookie +
+        self.send_header('cookie: '+ran_str_cookie +
                          '\n'+'Content-type', 'application/json')
-        print('cookie: '+self.ran_str_cookie+'\n' +
-              'Content-type', 'application/json')
         self.end_headers()
+        schookCheck = checkSchoolServer()
+        gol.set_schoolCheck(schookCheck, ran_str_cookie)
         # 发送维护的验证码
-        self.wfile.write(gol.get_schoolCheck().valcode.content)
+        self.wfile.write(gol.get_schoolCheck(ran_str_cookie).valcode.content)
 
     # 处理post请求
     def do_POST(self):
-        print("收到来自:"+self.client_address[0]+"的连接")
-        head = str(self.headers)
-        print(head)
-        # 寻找头部cookie并与本地匹配，验证成功才响应
-        print(head[head.find('Cookie')+8:head.find('Cookie')+32])
-        if head.find('Cookie') == -1:
-            return "No cookie"
-        elif head[head.find('Cookie')+8:head.find('Cookie')+32] != self.ran_str_cookie:
-            return "Wrong cookie"
-        # 回复响应头部
-        self.send_response(301)
-        self.send_header('Content-type', 'application/json')
+        global mysqlutil
+        self.send_response(200)
+        self.send_header('Content-type', 'json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
+        head = str(self.headers)
+        # 寻找头部cookie并与本地匹配，验证成功才响应
+        if head.find('Cookie') == -1:
+            print("No cookie")
+            #self.wfile.write(str("No cookie").encode('utf-8'))
+            # return "No cookie"
+        flag=True
+        ran_str_cookie=''
+        for key in dic_cookieHandler[self.client_address[0]]: # 在键值对中寻找对应的正确cookie
+            ran_str_cookie = str(key)
+            if head[head.find('Cookie')+8:head.find('Cookie')+32] == ran_str_cookie:
+                print(ran_str_cookie)
+                flag=False
+                break
+        if flag:
+            print("Wrong cookie")
+            # self.wfile.write(str("Wrong cookie").encode('utf-8'))
+            # return "Wrong cookie"
+        # 回复响应头部
         try:
             # 获取客户端传输数据
             length = int(self.headers['Content-Length'])
             data = self.rfile.read(length).decode('utf-8')
-            print(data)
-            Js = json.loads(data)
+            try:
+                Js = json.loads(data) # 尝试解析为json对象
+            except:
+                Js = data 
             print(Js)
             # 建立sql数据库对象
             global mysqlutil
-            mysqlutil = MysqlUtil()
             # 获取处理结果
-            respond = mysqlutil.mysqlAll(Js)
+            respond = mysqlutil.mysqlAll(Js, ran_str_cookie)
             print(respond)
             # 反馈结果
             self.wfile.write(str(respond).encode('utf-8'))
         except Exception as e:
-            mysqlutil = MysqlUtil()
             self.wfile.write("json error ".encode('utf-8'))
             print("json error "+str(e))
+            mysqlutil = MysqlUtil()
 
 
 if __name__ == '__main__':
-    mysqlutil = MysqlUtil()
     gol._init()
     # 多线程的TCP服务端，可以同时开启多个任务等着客户端来连，来一个请求就处理一个
     s = HTTPServer(("", 8089), MyServer)
